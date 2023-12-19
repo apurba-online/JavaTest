83eed6e0c97b2f45fdebfb048256827e6d6899d3
Md Shifatul Ahsan Apurba <apurbaahsan@gmail.com>
2023-12-19 20:11:03 +0600

    Update BasicJavaExample.java

--- BasicJavaExample.java
+++ BasicJavaExample.java
@@ -2,10 +2,12 @@ public class BasicJavaExample {
 
     public static void main(String[] args) {
         int number = 5;
-        displayNumber(number);
+        String message = "This is a message.";
+        displayNumber(number, message);
     }
   
-    public static void displayNumber(int num) {
+    public static void displayNumber(int num, String msg) {
         System.out.println("The number is: " + num);
+        System.out.println("Additional message: " + msg);
     }
 }
