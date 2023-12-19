f8fa677aea11d998984bea5dc5134b6b089c73a6
Md Shifatul Ahsan Apurba <apurbaahsan@gmail.com>
2023-12-19 20:08:33 +0600

    Create BasicJavaExample.java

---
+++ BasicJavaExample.java
@@ -0,0 +1,11 @@
+public class BasicJavaExample {
+
+    public static void main(String[] args) {
+        int number = 5;
+        displayNumber(number);
+    }
+  
+    public static void displayNumber(int num) {
+        System.out.println("The number is: " + num);
+    }
+}
