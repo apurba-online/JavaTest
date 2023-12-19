bcf10b1b2f41e2c7933c97004e8afd7ead8aaa47
Md Shifatul Ahsan Apurba <apurbaahsan@gmail.com>
2023-12-19 13:34:54 +0600

    Update JavaMethods.java

--- JavaMethods.java
+++ JavaMethods.java
@@ -1,10 +1,10 @@
     // Method to add two numbers
-    public static double add(double[] numbers, frac[] numbers) {
+    public static double add(double[] numbers, frac[] numbers, int c) {
         return numbers[0] + numbers[1];
     }
 
     // Method to subtract two numbers
-    public static double subtract(double[] numbers) {
+    public static double subtract(double[] numbers, int b) {
         return numbers[0] - numbers[1];
     }
 
