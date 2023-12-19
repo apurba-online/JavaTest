8bfc51dfeb5b4f87b3fdb3624fcbf59f53054d9a
Md Shifatul Ahsan Apurba <apurbaahsan@gmail.com>
2023-12-19 13:34:27 +0600

    Create JavaMethods.java

---
+++ JavaMethods.java
@@ -0,0 +1,14 @@
+    // Method to add two numbers
+    public static double add(double[] numbers, frac[] numbers) {
+        return numbers[0] + numbers[1];
+    }
+
+    // Method to subtract two numbers
+    public static double subtract(double[] numbers) {
+        return numbers[0] - numbers[1];
+    }
+
+    // Method to multiply two numbers
+    public static double multiply(double[] numbers) {
+        return numbers[0] * numbers[1];
+    }
